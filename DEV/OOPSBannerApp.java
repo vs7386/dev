b6cd1233016373b0bba8c8082e85ac83d59d00c6
moonbreakers <moonbreakers@moonbreakerss-MacBook-Pro.local>
2026-02-14 18:46:54 +0530

    UC2: Render OOPS as Banner using Print Statements

---
+++ DEV/OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class displays the OOPS acronym to the console.
+ *
+ * @author Srirangam Venkata Shrawan
+ * @version 21.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
---
+++ DEV/OOPSBannerApp2.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerApp UC2 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays
+ * the Object-Oriented Programming System (OOPS) acronym
+ * in a banner format using asterisks (*) and spaces.
+ *
+ * @author Srirangam Venkata Shrawan
+ * @version 21.0
+ */
+
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ******** " + "  ******** " + "  ******** " + "  ******** ");
+        System.out.println(" *      * " + "  *      * " + "  *      * " + "  *        ");
+        System.out.println(" *      * " + "  *      * " + "  *      * " + "  *        ");
+        System.out.println(" *      * " + "  *      * " + "  ******** " + "  ******** ");
+        System.out.println(" *      * " + "  *      * " + "  *        " + "         * ");
+        System.out.println(" *      * " + "  *      * " + "  *        " + "         * ");
+        System.out.println(" ******** " + "  ******** " + "  *        " + "  ******** ");
+    }
+}
